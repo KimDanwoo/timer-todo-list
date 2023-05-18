15db5f4c43f08afe53dc4a9c52a21f709e34fa5e
KimDanwoo <84695769+KimDanwoo@users.noreply.github.com>
2023-05-19 08:35:08 +0900

    ✨ localstorage 데이터 저장 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Home from "./pages/Home.js";
+import { getItem, setItem, removeItem } from "./utils/storage.js";
 
 export default function App({ $target }) {
   $target.style = "max-height:100vh; overflow: auto;";
 
   const onClickAddTodoItem = (payload) => {
-    console.log(payload);
-    console.log("ddddd");
+    const getTodos = getItem("todo", []);
+    const newPayload = { ...payload, date: Date.now() };
+    console.log(getTodos);
+    const todos = [...getTodos, newPayload];
+    setItem("todo", todos);
   };
 
   const home = new Home({
