75f92d3cee516f765f48dc330a9d09e6476ae897
KimDanwoo <84695769+KimDanwoo@users.noreply.github.com>
2023-05-19 09:19:39 +0900

    ✨  store 코드 추가

--- src/components/common/TodoList.js
+++ src/components/common/TodoList.js
@@ -1,13 +1,25 @@
+import { getItem } from "../../utils/storage";
 import FilterControl from "./FilterControl";
 import SubTitle from "./SubTitle";
 import TodoItem from "./TodoItem";
+import { store } from "../../store/index";
+import Button from "./Button";
 
-export default function TodoList({ $target }) {
+export default function TodoList({ $target, initialState }) {
   const $todoList = document.createElement("div");
   $todoList.style = "margin-top:28px";
   const $todoItems = document.createElement("div");
   $todoItems;
   $todoItems.classList.add("todo_list");
+
+  this.state = initialState;
+
+  const list = getItem("todo");
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
 
   new SubTitle({
     $target: $todoList,
@@ -18,25 +30,33 @@ export default function TodoList({ $target }) {
     $target: $todoList,
   });
 
-  new TodoItem({
-    $target: $todoItems,
-    title: "할 일 내용 1",
-    time: 60,
-  });
-  new TodoItem({
-    $target: $todoItems,
-    title: "할 일 내용 1",
-    time: 60,
-  });
-  new TodoItem({
-    $target: $todoItems,
-    title: "할 일 내용 1",
-    time: 60,
+  list.map((item) => {
+    new TodoItem({
+      $target: $todoItems,
+      title: item.title,
+      time: item.time,
+    });
   });
 
-  this.setState = () => {
-    this.render();
-  };
+  new TodoItem({
+    $target: $todoItems,
+    title: store.state.a,
+    time: store.state.b,
+  });
+
+  // new Button({
+
+  // })
+  // new TodoItem({
+  //   $target: $todoItems,
+  //   title: "할 일 내용 1",
+  //   time: 60,
+  // });
+  // new TodoItem({
+  //   $target: $todoItems,
+  //   title: "할 일 내용 1",
+  //   time: 60,
+  // });
 
   this.render = () => {
     $todoList.append($todoItems);
